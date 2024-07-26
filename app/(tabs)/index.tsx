ea3f4777d20b16f0db8664cd84464bed4c382ae9
abhishek9111 <42829419+Abhishek9111@users.noreply.github.com>
2024-07-26 23:23:16 +0530

    initial bootstraping-add headerbar and searchbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,82 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
+import { View, Text, ScrollView, StyleSheet, TextInput } from "react-native";
+import {
+  UserCircleIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView style={styles.container}>
+      <TopTabBar />
+      <UserSearchBar />
+    </ScrollView>
+  );
+}
+
+function TopTabBar() {
+  return (
+    <View style={styles.headerBar}>
+      <Text>Street food</Text>
+      <UserCircleIcon />
+    </View>
+  );
+}
+
+function UserSearchBar() {
+  return (
+    <View style={styles.searchContainer}>
+      <View style={styles.searchWrapper}>
+        <MagnifyingGlassIcon style={styles.iconStyle} />
+        <TextInput placeholder="Search" style={{ marginLeft: 6 }} />
+      </View>
+      {/* style={styles.input} */}
+      {/* value={searchPhrase} */}
+      {/* onChangeText={setSearchPhrase}
+      onFocus=
+      {() => {
+        setClicked(true);
+      }} */}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flexDirection: "column",
+    gap: 4,
+    backgroundColor: "white",
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  headerBar: {
+    flexDirection: "row",
+    backgroundColor: "#d1d5db",
+    flex: 1,
+    color: "black",
+    minHeight: 50,
+    justifyContent: "space-between",
+    padding: 16,
+    alignItems: "center",
+    marginBottom: 5,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  searchContainer: {
+    flexDirection: "row",
+    flex: 1,
+    marginHorizontal: 16,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderColor: "black",
+    color: "white",
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+    color: "black",
+  },
+  searchWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 4,
   },
 });
